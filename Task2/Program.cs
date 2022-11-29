bfa4b0ea2cd55fa0782c4437ae642b7adbf50f92
Malibu <malibu090@mail.ru>
2022-11-29 20:20:37 +0300

    max of two numbers

---
+++ Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+Console.Clear();
+Console.Write("Введите первое число ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число ");
+int numberB = int.Parse(Console.ReadLine());
+if (numberA > numberB)
+{
+    Console.WriteLine($"{numberA} is max");
+}
+else
+{
+    Console.Write($"{numberB} is max");
+}
